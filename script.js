e2e2b1824d34c78a2d5993940167bc0e045945b0
Jacob Lockhart <procoder165@gmail.com>
2022-11-07 12:23:20 -0500

    able to highlight things on page

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const board = document.querySelector("#gameBoard");
 
+
+//Dynamic creation of game board
 const makeGrid = (rows, cols) => {
   board.style.setProperty('--grid-rows', rows);
   board.style.setProperty('--grid-cols', cols);
 
   for (let i = 0; i < (rows * cols); i++){
     let cell = document.createElement("div");
+    // cell.innerText = (i + 1);
     board.appendChild(cell).className = "cell";
   }  
 }
 
+const hightlight = (e) => {
+  e.preventDefault();
+  const cell = e.target
+  console.log(cell);
+
+  cell.style.setProperty("background-color", "red");
+
+}
+
+
+
 makeGrid(20, 20)
+
+board.addEventListener("click", hightlight)
+
