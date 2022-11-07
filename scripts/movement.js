e2ed68805ef6d048dbcfc66351be3c5adcd38136
Jacob Lockhart <procoder165@gmail.com>
2022-11-07 14:50:47 -0500

    able to move up with top collision

--- scripts/movement.js
+++ scripts/movement.js
@@ -6,37 +6,60 @@ let downInterval;
 let leftInterval;
 let rightInterval;
 
-
+//Moving the snake up
 const moveUp = () => {
-
+  clearInterval(downInterval)
+  clearInterval(leftInterval)
+  clearInterval(rightInterval)
+  //9 X 9 gameboard
   if (total == 81) {
-    
-    if (activeCell - 9 < 0){
+    if (activeCell - 9 < 0) {
       board.children[activeCell].setAttribute("style", "background-color: #343536; color: #343536")
-      activeCell = 40
+      activeCell = parseInt((9 * 9) / 2)
       board.children[activeCell].setAttribute("style", "background-color: red; color: red")
       clearInterval(upInterval)
     }
-    else{
-    board.children[activeCell].setAttribute("style", "background-color: #343536; color: #343536")
+    else {
+      board.children[activeCell].setAttribute("style", "background-color: #343536; color: #343536")
 
-    activeCell -= 9;
-    board.children[activeCell].setAttribute("style", "background-color: red; color: red")
+      activeCell -= 9;
+      board.children[activeCell].setAttribute("style", "background-color: red; color: red")
     }
   }
-  else if (total == 225) {
-    board.children[activeCell].setAttribute("style", "background-color: #343536; color: #343536");
 
+  // 15 X 15 gameboard
+  else if (total == 225) {
+    if (activeCell - 15 < 0) {
+      board.children[activeCell].setAttribute("style", "background-color: #343536; color: #343536")
+      activeCell = parseInt((15 * 15) / 2)
+      board.children[activeCell].setAttribute("style", "background-color: red; color: red")
+      clearInterval(upInterval)
+    }
+
+    board.children[activeCell].setAttribute("style", "background-color: #343536; color: #343536");
     activeCell -= 15;
     board.children[activeCell].setAttribute("style", "background-color: red; color: red")
   }
+
+  //21 X 21 gameboard
+
+
   else if (total == 441) {
+
+    if (activeCell - 21 < 0) {
+      board.children[activeCell].setAttribute("style", "background-color: #343536; color: #343536")
+      activeCell = parseInt((21 * 21) / 2)
+      board.children[activeCell].setAttribute("style", "background-color: red; color: red")
+      clearInterval(upInterval)
+    }
     board.children[activeCell].setAttribute("style", "background-color: #343536; color: #343536");
 
     activeCell -= 21;
     board.children[activeCell].setAttribute("style", "background-color: red; color: red")
   }
 }
+
+
 
 const moveDown = () => {
   console.log("down");
